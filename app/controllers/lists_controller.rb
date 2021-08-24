a26b281b9db6d422f27568c28afd3446bd74f789
Dan <DanielMooreRealPerson@gmail.com>
2021-08-24 18:37:25 -0400

    fixed controller issues that were causing an error when trying to create new lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,35 +1,36 @@
 class ListsController < ApplicationController
     def index
-        lists = List.all
-        render json: lists, status: 200
+        @lists = List.all
+        render json: @lists, status: 200
     end
 
     def show
-        list = List.find(params[:id])
-        render json: list, status: 200
+        @list = List.find(params[:id])
+        render json: @list, status: 200
     end
     
     def create
-        list = List.create(list_params)
-        if list
-            render json: list
+        #binding.pry
+        @list = List.create(list_params)
+        if @list
+            render json: @list
         else
             render json: {error: 'This list could not be created.'}
     end
 
     def update
-        list = list.find(params[:id])
-        list.update(list_params)
-        if list
-            render json: list
+        @list = list.find(params[:id])
+        @list.update(list_params)
+        if @list
+            render json: @list
         else
             render json: {error: 'This list could not be edited.'}
         end
     end
 
     def destroy
-        list = List.find(params[:id])
-        list.destroy
+        @list = List.find(params[:id])
+        @list.destroy
         render json: {message: 'Event successfully deleted.'}
     end
 
